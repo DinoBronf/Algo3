d2f3c6f673da901e4106be2d7bf6e65a614fc62a
DinoBronf <142249369+DinoBronf@users.noreply.github.com>
2024-08-31 17:15:55 -0300

    Update Práctica1.cpp

--- Práctica1.cpp
+++ Práctica1.cpp
@@ -25,9 +25,9 @@ std::tuple<int, int> min_prioridades(const std::tuple<int, int>& t1, const std::
     int billetes1 = std::get<1>(t1);
     int billetes2 = std::get<1>(t2);
     if (suma1 > suma2)
-        return t1;
-    if (suma2 < suma1)
         return t2;
+    if (suma2 > suma1)
+        return t1;
     else 
         if (billetes1 >= billetes2)
             return t2;
@@ -38,17 +38,28 @@ std::tuple<int, int> min_prioridades(const std::tuple<int, int>& t1, const std::
 std::tuple<int, int> CC(std::vector<int>& B, int c) {
     int ultimo = B.back();
     if (c <= 0)
-        return std::make_tuple(suma, billetes);
+        return std::make_tuple(0, 0);
     if (B.empty())
         return std::make_tuple(INF,INF);
     else {
-        B.pop_back();
-        return min_prioridades(sumTuples(CC(B,c-ultimo), make_tuple(ultimo, 1)), CC(B,c)); 
+        std::vector<int> B_copy = B;
+         B_copy.pop_back();
+        return min_prioridades(sumTuples(CC(B_copy,c-ultimo), make_tuple(ultimo, 1)), CC(B_copy,c)); 
     }
 }
 
 
+int main() {
+    vector<int> B = {2, 3, 5, 10, 20, 20};
+    int c = 14;
+    
+    // Imprimir el resultado
+    auto result = CC(B, c);
+    cout << "La mínima suma es: " << get<0>(result) << endl;
+    cout << "Número de billetes: " << get<1>(result) << endl;
 
+    return 0;
+}
 
 
 //   if (suma1 > suma2)
